bec5b8c69043c3fd7b44b7729f113f5f0b644810
unknown <ahmadaqilanubala@gmail.com>
2026-07-27 06:38:36 +0700

    add test route for debugging

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\LoanController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test123', fn () => 'halo dunia');
+
 Route::get('/', [BookController::class, 'index'])->name('home');
 
 Route::middleware('auth')->group(function () {
@@ -19,14 +21,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', function () {
         return redirect()->route('home');
     })->name('dashboard');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
-    Route::post('/books/{book}/pinjam', [LoanController::class, 'store'])->name('loans.store');
-    Route::get('/pinjaman-saya', [LoanController::class, 'index'])->name('loans.index');
 });
 
 require __DIR__.'/admin.php';
