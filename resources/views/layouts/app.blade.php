1221c735d77a04038e79e9939fa58b91d66e1e8d
unknown <ahmadaqilanubala@gmail.com>
2026-07-27 20:26:32 +0700

    update footer text

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
                     <div class="flex flex-col sm:flex-row justify-between items-center gap-2 text-sm text-gray-500">
                         <span>&copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.</span>
-                        <span>Dibuat dengan &hearts; untuk pecinta buku.</span>
+                        <span>Ahmad Aqila Nubala | 41.24.0001.</span>
                     </div>
                 </div>
             </footer>
